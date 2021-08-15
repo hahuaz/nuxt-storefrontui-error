45d69d96e5398ebff2bb003a2da776c2bc196c02
hahuaz <hahuaz@gmail.com>
2021-08-15 17:26:24 +0300

    init repo

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <SfSteps v-model="active" :steps="steps" :can-go-back="canGoBack">
+      <SfStep v-for="(step, key) in steps" :key="key" :name="step">
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 18.75rem;
+            background-color: #f2f2f2;
+          "
+        >
+          {{ step }}
+        </div>
+      </SfStep>
+    </SfSteps>
+  </div>
+</template>
+<script>
+import { SfSteps } from '@storefront-ui/vue'
+export default {
+  components: {
+    SfSteps,
+  },
+  data() {
+    return {
+      active: 0,
+      steps: ['Personal details', 'Shipping', 'Billing address'],
+      canGoBack: true,
+    }
+  },
+}
+</script>
